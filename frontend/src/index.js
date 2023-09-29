4280f75947e54452dd10d51b7d887de0867145e3
SavaOurl <dendiroblek@gmail.com>
2023-09-29 23:25:08 +0400

    Added strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,9 @@ import './style/style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
